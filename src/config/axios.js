35b37dc54ae3f528dc456f6d26ffc9ae021f06ab
MfonAbasi Isaac <mfonabasiisaac@gmail.com>
2022-08-15 13:49:45 +0100

    Update

--- src/config/axios.js
+++ src/config/axios.js
@@ -5,6 +5,7 @@ import Cookie  from "js-cookie";
 // http://127.0.0.1:5000/api
 export default axios.create({
     baseURL:"http://testapp2.studyhub.ng/api",
+    withCredentials : true,
     headers: {
         'Content-Type' : 'application/json',
         "Access-Control-Allow-Origin": "*",
